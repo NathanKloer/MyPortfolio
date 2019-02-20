42ec8f3f3dd544a4acdf219d0ec319a4e7798e3c
Nathan <nathan.kloer@gmail.com>
2019-02-20 12:38:23 -0500

    fully working portfolio site

--- js/scrolling.js
+++ js/scrolling.js
@@ -16,6 +16,24 @@ $("#fileRequest").click(function() {
     window.location = 'resume.pdf';
 });
 
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+
 var config = {
     apiKey: "AIzaSyDvbnQTRiFLITcQGysvKWmiOTb4s2iSIwo",
     authDomain: "myportfolio-76385.firebaseapp.com",
