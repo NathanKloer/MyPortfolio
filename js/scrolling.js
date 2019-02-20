8682a5cdd5d88d9308a7be8b511155b2f83e42b3
Nathan <nathan.kloer@gmail.com>
2019-02-20 12:16:38 -0500

    message system working, fixed minor resume bugs

--- js/scrolling.js
+++ js/scrolling.js
@@ -16,92 +16,50 @@ $("#fileRequest").click(function() {
     window.location = 'resume.pdf';
 });
 
-$(document).ready(function() {
-    $("#submit").click(function() {
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-    var contact = $("#contact").val();
-    $("#returnmessage").empty(); // To empty previous error/success message.
-    // Checking for blank fields.
-    if (name == '' || email == '' || contact == '') {
-    alert("Please Fill Required Fields");
-    } else {
-    // Returns successful data submission message when the entered information is stored in database.
-    $.post("contact_form.php", {
-    name1: name,
-    email1: email,
-    message1: message,
-    contact1: contact
-    }, function(data) {
-    $("#returnmessage").append(data); // Append returned message to message paragraph.
-    if (data == "Your Query has been received, We will contact you soon.") {
-    $("#form")[0].reset(); // To reset form fields on success.
-    }
-    });
-    }
-    });
-    });
+var config = {
+    apiKey: "AIzaSyDvbnQTRiFLITcQGysvKWmiOTb4s2iSIwo",
+    authDomain: "myportfolio-76385.firebaseapp.com",
+    databaseURL: "https://myportfolio-76385.firebaseio.com",
+    projectId: "myportfolio-76385",
+    storageBucket: "myportfolio-76385.appspot.com",
+    messagingSenderId: "484788336806"
+  };
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
+  // Button for adding trains
+  $("#add-msg-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // User input
+    var nameInput = $("#name").val().trim();
+    var emailInput = $("#email").val().trim();
+    var subjectInput = $("#sub").val().trim();
+    var messageInput = $("#message").val().trim();
+  
+    // Creates "temporary" object for train data
+    var newMsg = {
+      name: nameInput,
+      email: emailInput,
+      subject: subjectInput,
+      message: messageInput
+    };
+  
+    // Uploads train data to the database
+    database.ref().push(newMsg);
+  
+    // Clears userinput text-boxes
+    $("#name").val("");
+    $("#email").val("");
+    $("#sub").val("");
+    $("#message").val("");
 
-    $(function()
-{
-    function after_form_submitted(data)
-    {
-        if(data.result == 'success')
-        {
-            $('form#reused_form').hide();
-            $('#success_message').show();
-            $('#error_message').hide();
-        }
-        else
-        {
-            $('#error_message').append('<ul></ul>');
+    $( "#success" ).trigger( "alert" );
 
-            jQuery.each(data.errors,function(key,val)
-            {
-                $('#error_message ul').append('<li>'+key+':'+val+'</li>');
-            });
-            $('#success_message').hide();
-            $('#error_message').show();
-
-            //reverse the response on the button
-            $('button[type="button"]', $form).each(function()
-            {
-                $btn = $(this);
-                label = $btn.prop('orig_label');
-                if(label)
-                {
-                    $btn.prop('type','submit' );
-                    $btn.text(label);
-                    $btn.prop('orig_label','');
-                }
-            });
-
-        }//else
-    }
-
-	$('#reused_form').submit(function(e)
-      {
-        e.preventDefault();
-
-        $form = $(this);
-        //show some response on the button
-        $('button[type="submit"]', $form).each(function()
-        {
-            $btn = $(this);
-            $btn.prop('type','button' );
-            $btn.prop('orig_label',$btn.text());
-            $btn.text('Sending ...');
-        });
-
-
-                    $.ajax({
-                type: "POST",
-                url: 'handler.php',
-                data: $form.serialize(),
-                success: after_form_submitted,
-                dataType: 'json'
-            });
-
-      });
+  });
+  $(document).ready(function(){
+    $("#add-msg-btn").click(function(){
+        $('.alert').show()
+    }) 
 });
